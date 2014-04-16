aed06ce43869e650c48be7f061506cc4621586d4
Nathan Batson <nabatson@gmail.com>
2014-04-16 19:39:08 -0400

    updated with full ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,15 @@
 class Beer < ActiveRecord::Base
+  has_many :ratings
+
+  def number_rating
+    average = 0
+
+    return average if ratings.length == 0
+
+    ratings.each do |rating|
+      average += rating.score
+    end
+
+    return average / ratings.length
+  end
 end
